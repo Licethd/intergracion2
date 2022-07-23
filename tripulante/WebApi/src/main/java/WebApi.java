67bdb6dbd9490f9a3aff94184d36eb98caec5e4f
Licethd <liceth.dfe@gmail.com>
2022-07-22 23:16:15 -0400

    prueba2

--- tripulante/WebApi/src/main/java/WebApi.java
+++ tripulante/WebApi/src/main/java/WebApi.java
@@ -3,10 +3,11 @@ import fourteam.http.Rest;
 
 public class WebApi {
 
-	public static void AddControllers() {
-		Rest.addController(TripulanteController.class);
+	public static void AddControllers()
+	{
+				Rest.addController(TripulanteController.class);
 
-		Rest.start(8080);
+			Rest.start(8080);
 		Rest.createSwagger();
 	}
 
